4b3d93164914509055c37bad7c4ea0296e8ddf51
hadadnejadhirad-commits <hadadnejadhirad@gmail.com>
2026-08-16 19:08:01 +0330

    Add GitHub Pages React entry

---
+++ pages/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Home from "../app/page";
+import "../app/globals.css";
+import "../app/enhancements.css";
+import "./static.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Home />
+  </StrictMode>,
+);
